ee0cb28361435943e569aed6345ad836b1f3650c
mandreae <melissa.andreae@sourcecoast.com>
2016-06-14 10:07:46 -0400

    SCLogin - add post text to social buttons #jfbconnect-1770 Fixed

--- tmpl/socialLogin.php
+++ tmpl/socialLogin.php
@@ -17,10 +17,13 @@ $loginButtons = $helper->getLoginButtons($orientation, $alignment);
 if ($loginButtons != '')
 {
     $introText = JText::_('MOD_SCLOGIN_SOCIAL_INTRO_TEXT_LABEL');
+    $postText = JText::_('MOD_SCLOGIN_SOCIAL_POST_TEXT_LABEL');
 
     echo '<div class="sclogin-social-login '.$socialSpan . ' ' . $layout . ' ' . $orientation.'">';
     if($introText)
         echo '<span class="sclogin-social-intro '.$socialSpan.'">'.$introText.'</span>';
     echo $loginButtons;
+    if($postText)
+        echo '<span class="sclogin-social-post-text '.$socialSpan.'">'.$postText.'</span>';
     echo '</div>';
 }
